1960d4b0cc5f3ff564e2d60fed2bc61f4ee853b7
abrahamluiss <abraham105@hotmail.com>
2019-06-24 01:34:57 -0500

    agregando admin y agregar categorias

---
+++ AppWebInstrumentos/AppWebInstrumentos/Agregar_Categorias.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMaster.master" AutoEventWireup="true" CodeFile="Agregar_Categorias.aspx.cs" Inherits="Agregar_Categorias" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .auto-style1 {
+            height: 26px;
+        }
+        .auto-style2 {
+            height: 26px;
+            text-align: center;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width:100%;">
+        <tr>
+            <td class="auto-style1"></td>
+            <td class="auto-style2">Agregar Categorias</td>
+            <asp:Label ID="lbloperacion" runat="server" ForeColor="White"></asp:Label>
+            <td class="auto-style1"></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <asp:Button ID="btnNuevo" runat="server" Text="Nuevo" OnClick="btnNuevo_Click" />
+                <asp:Button ID="btnguardar" runat="server" Text="Guardar" OnClick="btnguardar_Click" />
+                <asp:Button ID="btncancelar" runat="server" Text="Cancelar" OnClick="btncancelar_Click" />
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1"></td>
+            <td class="auto-style1">idCategorias</td>
+            <td class="auto-style1">
+                <asp:TextBox ID="txtIDcat" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>nombrecategoria</td>
+            <td>
+                <asp:TextBox ID="txtnombrecat" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>descripcion</td>
+            <td>
+                <asp:TextBox ID="txtdescr" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+    <p>Todas las categorias</p>
+    <p>
+        <asp:GridView ID="grdcateg" runat="server">
+        </asp:GridView>
+    </p>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+    
+</asp:Content>
+
---
+++ AppWebInstrumentos/AppWebInstrumentos/Agregar_Categorias.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Agregar_Categorias : System.Web.UI.Page
+{
+    conexionALSE con = new conexionALSE();
+    consultasALSE datos = new consultasALSE();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        vercategorias();
+        deshabilitar(false);
+        btnguardar.Enabled = false;
+        btnNuevo.Enabled = true;
+    }
+    public void deshabilitar(Boolean estado)
+    {
+        txtIDcat.Enabled = estado;
+        txtnombrecat.Enabled = estado;
+        txtdescr.Enabled = estado;
+    }
+    public void vercategorias()
+    {
+        grdcateg.DataSource = datos.extrae("vercatg");
+        grdcateg.DataBind();
+    }
+    protected void btnNuevo_Click(object sender, EventArgs e)
+    {
+        lbloperacion.Text = "n";
+        limpiar();
+        deshabilitar(true);
+        btnguardar.Enabled = true;
+        //btnmodificar.Enabled = false;
+        btnNuevo.Enabled = false;
+        txtIDcat.Enabled = false;
+    }
+
+    protected void btnguardar_Click(object sender, EventArgs e)
+    {
+        if (lbloperacion.Text == "m")
+        {
+            //modificarcatg();
+            Response.Write("<Script>alert('Un no se puede Modificar');</script>");
+        }
+        else
+        {
+            insertarcatg();
+            Response.Write("<Script>alert('Marca Insertado');</script>");
+        }
+    }
+    public void insertarcatg()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con.cad;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "inscatg";
+        cmd.Parameters.Add("@nombrecategoria", SqlDbType.VarChar).Value = txtnombrecat.Text;
+        cmd.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = txtdescr.Text;
+        con.conectar();
+        cmd.ExecuteNonQuery();
+        con.desconectar();
+    }
+
+    protected void btncancelar_Click(object sender, EventArgs e)
+    {
+        btnguardar.Enabled = false;
+        btnNuevo.Enabled = true;
+        deshabilitar(true);
+        txtIDcat.Enabled = false;
+        limpiar();
+    }
+
+    public void limpiar()
+    {
+        txtIDcat.Text = string.Empty;
+        txtnombrecat.Text = string.Empty;
+        txtdescr.Text = string.Empty;
+    }
+}
