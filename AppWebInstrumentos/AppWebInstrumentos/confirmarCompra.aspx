4cdd54f847a5b2f4ccda8b65023c11181bdd9706
abrahamluiss <abraham105@hotmail.com>
2019-06-25 21:57:19 -0500

    Se agrego confirmar compra

---
+++ AppWebInstrumentos/AppWebInstrumentos/confirmarCompra.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterInstrumentos.master" AutoEventWireup="true" CodeFile="confirmarCompra.aspx.cs" Inherits="confirmarCompra" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="form-group" id="cccont">
+<div class="fila">
+    IdPedido
+    <asp:Label ID="lblidpedido" runat="server"></asp:Label>
+    <br />
+    </div>
+        <div class="fila">
+    IdCliente
+    <asp:Label ID="lblidcliente" runat="server"></asp:Label>
+    <br />
+            </div>
+        <div class="fila">
+    IdEmpleado
+    <asp:Label ID="lblidempleado" runat="server"></asp:Label>
+    <br />
+            </div>
+        <div class="fila">
+    FechaPedido
+    <asp:Label ID="lblfechapedido" runat="server"></asp:Label>
+    <br />
+            </div>
+        <div class="fila">
+    FechaEntrega
+    <asp:Label ID="lblfechaentrega" runat="server"></asp:Label>
+    <br />
+            </div>
+        <div class="fila">
+    FechaEnvio
+    <asp:Label ID="lblfechaenvio" runat="server"></asp:Label>
+    <br />
+            </div>
+        <div class="fila">
+    FormaEnvio
+    <asp:DropDownList ID="lstformaenvio" runat="server">
+    </asp:DropDownList>
+    <br />
+            </div>
+        <div class="fila">
+    Destinatario
+    <asp:DropDownList ID="lstdestinatario" runat="server" OnSelectedIndexChanged="lstdestinatario_SelectedIndexChanged" AppendDataBoundItems="True">
+    </asp:DropDownList>
+&nbsp;Cargo
+    <asp:Label ID="lblcargo" runat="server">1.00</asp:Label>
+    <br />
+    <br />
+            </div>
+        <div class="fila">
+    DireccionDestinatario
+    <asp:TextBox ID="txtdirecciondestinatario" runat="server"></asp:TextBox>
+    <br />
+            </div>
+        <div class="fila">
+    CiudadDestinatario
+    <asp:TextBox ID="txtciudaddestino" runat="server"></asp:TextBox>
+    <br />
+            </div>
+        <div class="fila">
+    RegionDestinatario
+    <asp:TextBox ID="txtregiondestinatario" runat="server"></asp:TextBox>
+    <br />
+            </div>
+        <div class="fila">
+    CodPostalDestinatario
+    <asp:TextBox ID="txtcodpostal" runat="server"></asp:TextBox>
+    <br />
+            </div>
+        <div class="fila">
+    PaisDestinatario&nbsp;&nbsp;
+    <asp:TextBox ID="txtpais" runat="server"></asp:TextBox>
+    <br />
+            </div>
+    <br />
+    Total a pagar
+    <asp:Label ID="lbltotalpagar" runat="server"></asp:Label>
+    <br />
+    <br />
+    <asp:Button ID="btnComprar" runat="server" OnClick="btnComprar_Click" Text="Efectuar Compra" />
+    <br />
+    <br />
+    <br />
+         </div>
+</asp:Content>
+
+
---
+++ AppWebInstrumentos/AppWebInstrumentos/confirmarCompra.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class confirmarcompra : System.Web.UI.Page
+{
+
+    consultasALSE datos = new consultasALSE();
+    consultasALSE datos2 = new consultasALSE();
+    consultasALSE datos3 = new consultasALSE();
+    consultasALSE datos4 = new consultasALSE();
+    conexionALSE cn = new conexionALSE();
+    DateTime fecha = DateTime.Now;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string idUsuario = Session["iduser"].ToString();
+        if (idUsuario.Trim().Length > 3)
+        {
+            mostrarDatos(idUsuario);
+            monto(Convert.ToInt32(Session["idpedido"].ToString()));
+        }
+    }
+
+    public void mostrarDatos(string idusuar)
+    {
+        DataTable dt = new DataTable();
+        lblidpedido.Text = Session["idpedido"].ToString();
+        lblidcliente.Text = Session["iduser"].ToString();
+        lblidempleado.Text = "NNN";
+
+        lblfechapedido.Text = fecha.ToString();
+        lblfechaentrega.Text = fecha.AddDays(2).ToString();
+        lblfechaenvio.Text = fecha.AddDays(1).ToString();
+        lstformaenvio.DataSource = datos.extrae("getListaciaenvio");
+        lstformaenvio.DataTextField = "NombreCompañia";
+        lstformaenvio.DataValueField = "idCompañiaEnvios";
+        lstformaenvio.DataBind();
+        dt = datos4.extrae("getListadestinatarios");
+        lstdestinatario.DataSource = dt;
+        lstdestinatario.DataTextField = "nom";
+        lstdestinatario.DataValueField = "NombreContacto";
+        lstdestinatario.DataBind();
+
+
+
+    }
+    public void monto(int idtemp)
+    {
+        DataTable dt2 = new DataTable();
+        dt2 = datos2.extrae("montopedido", "@idpedido", idtemp);
+        lbltotalpagar.Text = dt2.Rows[0][0].ToString();
+    }
+
+
+    protected void lstdestinatario_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    public void rellenarxdestinatario()
+    {
+        DataTable dt3 = new DataTable();
+        dt3 = datos3.extrae(lstdestinatario.SelectedItem.Value.ToString(), "@nombrecont", "getListadestinatariosxid");
+        txtdirecciondestinatario.Text = dt3.Rows[0][4].ToString();
+        txtciudaddestino.Text = dt3.Rows[0][5].ToString();
+        txtregiondestinatario.Text = dt3.Rows[0][6].ToString();
+        txtcodpostal.Text = dt3.Rows[0][7].ToString();
+        txtpais.Text = dt3.Rows[0][8].ToString();
+    }
+
+    protected void btnComprar_Click(object sender, EventArgs e)
+    {
+        int newIdPedido = insertapedido();
+        //insertar detalles de pedido
+
+        insertadetalle(newIdPedido);
+
+        Session["iduser"] = "ANTON";
+        Response.Write("<Script>alert('Compra realizada');window.location.href='reportepedido.aspx'</script>");
+        //Response.Redirect("~/reportepedido");
+
+    }
+    public void insertadetalle(int idpedidonuevo)
+    {
+        //        ins_detalle
+        //@idpedidonuevo int,
+        //@idpedidotemp int
+
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        cmd.Connection = cn.cad;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "ins_detalle";
+        cmd.Parameters.Add("@idpedidonuevo", SqlDbType.Int).Value = idpedidonuevo;
+        cmd.Parameters.Add("@idpedidotemp", SqlDbType.Int).Value = Convert.ToInt32(Session["idpedido"].ToString());
+        cmd.Connection.Open();
+        cmd.ExecuteNonQuery();
+        cmd.Connection.Close();
+
+    }
+
+    public int insertapedido()
+    {
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        cmd.Connection = cn.cad;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "pedido_ins";
+        SqlParameter codigo = new SqlParameter("@idpedido", SqlDbType.Int);
+        codigo.Direction = ParameterDirection.Output;
+        cmd.Parameters.Add(codigo);
+        cmd.Parameters.Add("@IdCliente", SqlDbType.NChar).Value = Session["iduser"];
+        cmd.Parameters.Add("@IdEmpleado", SqlDbType.Int).Value = 1;
+        cmd.Parameters.Add("@FechaPedido", SqlDbType.DateTime).Value = fecha;
+        cmd.Parameters.Add("@FechaEntrega", SqlDbType.DateTime).Value = fecha.AddDays(2);
+        cmd.Parameters.Add("@FechaEnvio", SqlDbType.DateTime).Value = fecha.AddDays(1);
+        cmd.Parameters.Add("@FormaEnvio", SqlDbType.Int).Value = int.Parse(lstformaenvio.SelectedItem.Value);
+        cmd.Parameters.Add("@cargo", SqlDbType.Decimal).Value = double.Parse(lblcargo.Text.ToString());
+        cmd.Parameters.Add("@Destinatario", SqlDbType.NVarChar).Value = lstdestinatario.SelectedItem.Value;
+        cmd.Parameters.Add("@DireccionDestinatario", SqlDbType.NVarChar).Value = txtdirecciondestinatario.Text.ToString();
+        cmd.Parameters.Add("@CiudadDestinatario", SqlDbType.NVarChar).Value = txtciudaddestino.Text.ToString();
+        cmd.Parameters.Add("@RegionDestinatario", SqlDbType.NVarChar).Value = txtregiondestinatario.Text.ToString();
+        cmd.Parameters.Add("@CodPostalDestinatario", SqlDbType.NVarChar).Value = txtcodpostal.Text.ToString();
+        cmd.Parameters.Add("@PaisDestinatario", SqlDbType.NVarChar).Value = txtpais.Text.ToString();
+        cmd.Connection.Open();
+        cmd.ExecuteNonQuery();
+        cmd.Connection.Close();
+        int idp = Convert.ToInt32(cmd.Parameters["@idpedido"].Value);
+        return idp;
+
+    }
+
+
+    protected void btnautocompletar_Click(object sender, EventArgs e)
+    {
+        rellenarxdestinatario();
+    }
+}
